b7497371bd2f5c62c0bca5eabedf97b99a33f5a3
Tanya <tanyashukla006@gmail.com>
2024-06-28 14:34:31 +0530

    workdir changed to userapp

--- apps/user-app/app/(dashboard)/p2pTransaction/page.tsx
+++ apps/user-app/app/(dashboard)/p2pTransaction/page.tsx
@@ -3,25 +3,25 @@ import { OnP2Ptransaction } from "../../../components/OnP2Ptransaction";
 import { getServerSession } from "next-auth";
 import prisma from "@repo/db/client";
 import { authOptions } from "../../lib/auth";
-
-export async function getP2Ptransactions() {
-    const session = await getServerSession(authOptions);
-    const txns = await prisma.p2pTransfer.findMany({
-        where: {
-            OR: [
-                { fromUserId: Number(session?.user?.id) },
-                { toUserId: Number(session?.user?.id) }
-            ]
+import { getP2Ptransactions } from "../../lib/actions/getP2P";
+// export async function getP2Ptransactions() {
+//     const session = await getServerSession(authOptions);
+//     const txns = await prisma.p2pTransfer.findMany({
+//         where: {
+//             OR: [
+//                 { fromUserId: Number(session?.user?.id) },
+//                 { toUserId: Number(session?.user?.id) }
+//             ]
            
-        }
-    });
-    return txns.map(t => ({
-        time: t.timeStamp,
-        amount: t.amount,
-        fromUserId: t.fromUserId,
-        toUserId: t.toUserId
-    }))
-}
+//         }
+//     });
+//     return txns.map(t => ({
+//         time: t.timeStamp,
+//         amount: t.amount,
+//         fromUserId: t.fromUserId,
+//         toUserId: t.toUserId
+//     }))
+// }
 export default async function() {
     const transactions = await getP2Ptransactions();
     const session = await getServerSession(authOptions);
